ab9c5000dae8cddd55173586f24c4193d0899038
Andre Barros <andrebarros7201@gmail.com>
2025-07-22 14:59:54 +0100

    create Community model

---
+++ server/Models/Community.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Verbum.API.Models;
+
+public class Community {
+    public int Id { get; set; }
+
+    [Required]
+    [MinLength(3)]
+    [MaxLength(20)]
+    public string Name { get; set; }
+
+    [Required] [StringLength(300)] public string Description { get; set; }
+    [Required] public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    [Required] public int UserId { get; set; }
+    [Required] public User Owner { get; set; }
+
+    // Relationships
+    public List<UserCommunity> Members { get; set; } = new();
+    public List<Post> Posts { get; set; } = new();
+}
