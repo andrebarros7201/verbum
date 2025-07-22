911b6d6e0f9b7a22668f2604f2c22e87f4613128
Andre Barros <andrebarros7201@gmail.com>
2025-07-22 21:37:57 +0100

    create interface IUserRepository

---
+++ server/Interfaces/Repositories/IUserRepository.cs
@@ -0,0 +1,10 @@
+using Verbum.API.Models;
+
+namespace Verbum.API.Interfaces.Repositories;
+
+public interface IUserRepository {
+    Task<User> GetUserByIdAsync(int id);
+    Task<User> AddAsync(User user);
+    Task<User> UpdateAsync(User user);
+    Task<bool> DeleteAsync(int id);
+}
