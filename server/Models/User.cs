197067ba6b43572109c74fd17e3858db3bb82f62
Andre Barros <andrebarros7201@gmail.com>
2025-07-22 14:59:42 +0100

    create User model

---
+++ server/Models/User.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Verbum.API.Models;
+
+public class User {
+    public int Id { get; set; }
+    [Required] [MinLength(3)] public string Username { get; set; }
+    [Required] [MinLength(6)] public string Password { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+    // Relationships
+    public List<UserCommunity> CommunitiesJoined { get; set; } = new();
+    public List<Post> Posts { get; set; } = new();
+    public List<Comment> Comments { get; set; } = new();
+    public List<Vote> Votes { get; set; } = new();
+}
