d033e99ab8cf72eec23ccc98efab2d37843b704e
Andre Barros <andrebarros7201@gmail.com>
2025-07-23 11:58:35 +0100

    set default values for Username and Password in CreateUserDto and UpdateUserDto

--- server/DTOs/User/UpdateUserDto.cs
+++ server/DTOs/User/UpdateUserDto.cs
@@ -3,6 +3,6 @@ using System.ComponentModel.DataAnnotations;
 namespace Verbum.API.DTOs.User;
 
 public class UpdateUserDto {
-    [Required] [MinLength(3)] public string Username { get; set; }
-    [Required] [MinLength(6)] public string Password { get; set; }
+    [Required] [MinLength(3)] public string Username { get; set; } = string.Empty;
+    [Required] [MinLength(6)] public string Password { get; set; } = string.Empty;
 }
