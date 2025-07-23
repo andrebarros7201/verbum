9f1e5068d3923d702d7d226bd96c3e19d4a575ed
Andre Barros <andrebarros7201@gmail.com>
2025-07-23 11:58:59 +0100

    register IUserRepository and UserRepository in DI container

--- server/Program.cs
+++ server/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Verbum.API.Data;
+using Verbum.API.Interfaces.Repositories;
+using Verbum.API.Repositories;
 
 namespace Verbum.API;
 
@@ -11,6 +13,8 @@ public class Program {
 
         builder.Services.AddDbContext<AppDbContext>(options =>
             options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+        builder.Services.AddScoped<IUserRepository, UserRepository>();
 
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
