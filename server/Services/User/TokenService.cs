70d764df460e1ebd8f20e5187ffb6e3fa849f829
Andre Barros <andrebarros7201@gmail.com>
2025-07-24 17:17:59 +0100

    implement TokenService for generating JWT tokens

---
+++ server/Services/User/TokenService.cs
@@ -0,0 +1,37 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using Verbum.API.DTOs.User;
+
+namespace Verbum.API.Services;
+
+public class TokenService {
+    private readonly IConfiguration _configuration;
+
+    public TokenService(IConfiguration configuration) {
+        _configuration = configuration;
+    }
+
+    public string GenerateToken(UserDto user) {
+        var handler = new JwtSecurityTokenHandler();
+        byte[] key = Encoding.ASCII.GetBytes(Configuration.JWT_SECRET);
+        var credentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature);
+        var tokenDescriptor = new SecurityTokenDescriptor {
+            Subject = GenerateClaimsIdentity(user),
+            SigningCredentials = credentials,
+            Expires = DateTime.UtcNow.AddDays(7)
+        };
+
+        var token = handler.CreateToken(tokenDescriptor);
+
+        return handler.WriteToken(token);
+    }
+
+    private static ClaimsIdentity GenerateClaimsIdentity(UserDto user) {
+        var ci = new ClaimsIdentity();
+        ci.AddClaim(new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()));
+        ci.AddClaim(new Claim(ClaimTypes.Name, user.Username));
+        return ci;
+    }
+}
