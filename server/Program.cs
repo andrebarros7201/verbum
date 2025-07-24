8c39eef437c9150284bb1d7037570dfbfb910c82
Andre Barros <andrebarros7201@gmail.com>
2025-07-24 15:32:11 +0100

    update DI container to register IUserService and UserService

--- server/Program.cs
+++ server/Program.cs
@@ -16,8 +16,9 @@ public class Program {
         builder.Services.AddDbContext<AppDbContext>(options =>
             options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-        builder.Services.AddScoped<IUserRepository, UserRepository>();
+        builder.Services.AddScoped<IUserService, UserService>();
         builder.Services.AddScoped<IAuthService, AuthService>();
+        builder.Services.AddScoped<IUserRepository, UserRepository>();
 
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
