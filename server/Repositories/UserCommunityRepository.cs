5b7fbe221dec69cd455e5c1bd24bb1fa51523691
Andre Barros <andrebarros7201@gmail.com>
2025-07-25 18:43:58 +0100

    add IUserCommunityRepository interface and UserCommunityRepository implementation for user-community management

---
+++ server/Repositories/UserCommunityRepository.cs
@@ -0,0 +1,31 @@
+using Verbum.API.Data;
+using Verbum.API.Interfaces;
+using Verbum.API.Models;
+
+namespace Verbum.API.Repositories;
+
+public class UserCommunityRepository : IUserCommunityRepository {
+
+    private readonly AppDbContext _db;
+
+    public UserCommunityRepository(AppDbContext db) {
+        _db = db;
+    }
+
+    public async Task<UserCommunity> GetUserCommunity(int userId, int communityId) {
+        return await _db.UserCommunities.FindAsync(userId, communityId);
+    }
+
+    public async Task<bool> AddUserToCommunity(UserCommunity uc) {
+        _db.UserCommunities.Add(uc);
+        await _db.SaveChangesAsync();
+        return true;
+    }
+
+    public async Task<bool> RemoveUserFromCommunity(UserCommunity uc) {
+        var userCommunity = await _db.UserCommunities.FindAsync(uc.UserId, uc.CommunityId);
+        _db.UserCommunities.Remove(userCommunity);
+        await _db.SaveChangesAsync();
+        return true;
+    }
+}
