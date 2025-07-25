7e9a2fcd2878fa64bc53995f43d92bffa2f76f61
Andre Barros <andrebarros7201@gmail.com>
2025-07-25 14:54:32 +0100

    add UpdateUser endpoint in UserController and append JWT to cookies on update

--- server/Controllers/User/UserController.cs
+++ server/Controllers/User/UserController.cs
@@ -1,19 +1,49 @@
 using System.Security.Claims;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Verbum.API.DTOs.User;
 using Verbum.API.Interfaces.Services;
+using Verbum.API.Services;
 
 namespace Verbum.API.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
 public class UserController : ControllerBase {
+    private readonly TokenService _tokenService;
     private readonly IUserService _userService;
 
-    public UserController(IUserService userService) {
+    public UserController(IUserService userService, TokenService tokenService) {
         _userService = userService;
+        _tokenService = tokenService;
     }
 
+    [Authorize]
+    [HttpPatch]
+    public async Task<IActionResult> UpdateUser([FromBody] UpdateUserDto dto) {
+        if (!ModelState.IsValid) {
+            return BadRequest(ModelState);
+        }
+
+        string? userClaimId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        if (userClaimId == null) {
+            return Unauthorized();
+        }
+
+        dto.Id = int.Parse(userClaimId);
+
+        var result = await _userService.UpdateUser(dto);
+        if (result == null) {
+            return NotFound(new { message = "User not found" });
+        }
+
+        string token = _tokenService.GenerateToken(result);
+
+        Response.Cookies.Append("token", token,
+            new CookieOptions { HttpOnly = true, Secure = true, Expires = DateTime.UtcNow.AddDays(7), SameSite = SameSiteMode.Strict });
+
+        return Ok();
+    }
 
     [Authorize]
     [HttpDelete]
--- server/DTOs/User/UpdateUserDto.cs
+++ server/DTOs/User/UpdateUserDto.cs
@@ -1,8 +1,10 @@
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace Verbum.API.DTOs.User;
 
 public class UpdateUserDto {
+    [JsonIgnore] public int Id { get; set; }
     [Required] [MinLength(3)] public string Username { get; set; } = string.Empty;
     [Required] [MinLength(6)] public string Password { get; set; } = string.Empty;
 }
