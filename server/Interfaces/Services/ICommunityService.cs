33cae88f7bbc22e3fe24ad0a3f24dd568d11ada9
Andre Barros <andrebarros7201@gmail.com>
2025-07-28 12:13:39 +0100

    update `IPostService` and `ICommunityService` to include `userId` parameter for enhanced user context, and add `PostVote` method to `IPostService`

--- server/Interfaces/Services/ICommunityService.cs
+++ server/Interfaces/Services/ICommunityService.cs
@@ -3,9 +3,9 @@ using Verbum.API.DTOs.Community;
 namespace Verbum.API.Interfaces.Services;
 
 public interface ICommunityService {
-    Task<CommunityCompleteDto?> GetCommunityById(int id);
-    Task<List<CommunitySimpleDto>> GetCommunities();
-    Task<List<CommunitySimpleDto>> GetCommunitiesByName(string name);
+    Task<CommunityCompleteDto?> GetCommunityById(int id, int userId);
+    Task<List<CommunitySimpleDto>> GetCommunities(int userId);
+    Task<List<CommunitySimpleDto>> GetCommunitiesByName(string name, int userId);
     Task<CommunitySimpleDto?> CreateCommunity(CreateCommunityDto dto, int userId);
     Task<CommunitySimpleDto?> UpdateCommunity(int userId, int communityId, UpdateCommunityDto dto);
     Task<bool> JoinCommunity(int communityId, int userId);
--- server/Interfaces/Services/IPostService.cs
+++ server/Interfaces/Services/IPostService.cs
@@ -3,8 +3,9 @@ using Verbum.API.DTOs.Post;
 namespace Verbum.API.Interfaces.Services;
 
 public interface IPostService {
-    Task<PostCompleteDto> GetPostById(int id);
+    Task<PostCompleteDto> GetPostById(int id, int userId);
     Task<PostSimpleDto> CreatePost(CreatePostDto dto, int userId);
     Task<PostSimpleDto> UpdatePost(int userId, int postId, UpdatePostDto dto);
     Task<bool> DeletePost(int userId, int postId);
+    Task<PostCompleteDto?> PostVote(int userId, int postId, int value);
 }
