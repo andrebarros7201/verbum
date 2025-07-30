bce5c68847ec99320f0ca39cdf203b1a6f0fc9a1
Andre Barros <andrebarros7201@gmail.com>
2025-07-30 17:37:16 +0100

    add `/register` route with `Register` page integration into routes configuration

--- client/src/routes.tsx
+++ client/src/routes.tsx
@@ -1,5 +1,17 @@
 import App from "./App";
+import { Register } from "./pages/Register.tsx";
 
-const routes = [{ path: "/", element: <App /> }];
+const routes = [
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/register",
+        element: <Register />,
+      },
+    ],
+  },
+];
 
 export { routes };
