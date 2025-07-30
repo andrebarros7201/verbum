aa2749de4444d2096bfe2772a51b7146ac0e042c
Andre Barros <andrebarros7201@gmail.com>
2025-07-30 17:36:52 +0100

    add `RegisterForm` component with username and password inputs and submission handling

---
+++ client/src/components/RegisterForm.tsx
@@ -0,0 +1,23 @@
+import { Form } from "./ui/Form.tsx";
+import { FormInput } from "./ui/Input.tsx";
+import { Button } from "./ui/Button.tsx";
+import type { FormEvent } from "react";
+
+const RegisterForm = () => {
+  function onSubmit(e: FormEvent) {
+    e.preventDefault();
+    console.log("submit");
+  }
+
+  return (
+    <main className={"w-full max-w-2xl flex justify-center"}>
+      <Form onSubmit={(e: FormEvent) => onSubmit(e)}>
+        <FormInput type={"text"} name={"username"} label={"Username"} />
+        <FormInput type={"password"} name={"password"} label={"Password"} />
+        <Button label={"Register"} variant={"primary"} type={"submit"} />
+      </Form>
+    </main>
+  );
+};
+
+export { RegisterForm };
