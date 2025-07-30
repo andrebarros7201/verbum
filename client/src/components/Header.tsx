d9c22b5db03b620c3122f0b2db91dc78bc8232a0
Andre Barros <andrebarros7201@gmail.com>
2025-07-30 20:03:56 +0100

    add `Login` button to `Header` component

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -10,6 +10,7 @@ const Header = () => {
       <h2 className={"italic font-bold text-2xl"}>Verbum</h2>
       <div className={"flex gap-4"}>
         <ButtonLink href={"/register"} label={"Register"} />
+        <ButtonLink href={"/login"} label={"Login"} variant={"primary"} />
       </div>
     </div>
   );
