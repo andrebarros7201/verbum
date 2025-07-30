96e35879c0fd4d806140817931f43c4635cc668f
Andre Barros <andrebarros7201@gmail.com>
2025-07-30 17:36:58 +0100

    add `Register` page with `RegisterForm` component integration

---
+++ client/src/pages/Register.tsx
@@ -0,0 +1,11 @@
+import { RegisterForm } from "../components/RegisterForm.tsx";
+
+const Register = () => {
+  return (
+    <main className={"w-full flex justify-center"}>
+      <RegisterForm />
+    </main>
+  );
+};
+
+export { Register };
