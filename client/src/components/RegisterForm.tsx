3fca3cd97143e4fdb08375dbc5ea25c0d8d43149
Andre Barros <andrebarros7201@gmail.com>
2025-07-30 19:59:12 +0100

    add `userRegister` and `userLogin` async thunks to `userSlice` and integrate registration flow with Redux

--- client/src/components/RegisterForm.tsx
+++ client/src/components/RegisterForm.tsx
@@ -1,23 +1,51 @@
 import { Form } from "./ui/Form.tsx";
 import { FormInput } from "./ui/Input.tsx";
 import { Button } from "./ui/Button.tsx";
-import type { FormEvent } from "react";
+import { type FormEvent, useRef } from "react";
+import { userRegister } from "../redux/slices/userSlice.ts";
+import { useDispatch } from "react-redux";
+import type { AppDispatch } from "../redux/store.ts";
 
 const RegisterForm = () => {
-  function onSubmit(e: FormEvent) {
+  const dispatch = useDispatch<AppDispatch>();
+  const usernameRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+
+  async function onSubmit(e: FormEvent) {
     e.preventDefault();
-    console.log("submit");
+    const username = usernameRef.current?.value;
+    const password = passwordRef.current?.value;
+
+    if (!username || !password) {
+      return;
+    }
+
+    try {
+      const response = await dispatch(
+        userRegister({ username, password }),
+      ).unwrap();
+      console.log(response);
+    } catch (e) {
+      console.log(e);
+    }
   }
 
   return (
     <main className={"w-full max-w-2xl flex justify-center"}>
-      <Form onSubmit={(e: FormEvent) => onSubmit(e)}>
-        <FormInput type={"text"} name={"username"} label={"Username"} min={3} />
+      <Form onSubmit={onSubmit}>
+        <FormInput
+          type={"text"}
+          name={"username"}
+          label={"Username"}
+          min={3}
+          ref={usernameRef}
+        />
         <FormInput
           type={"password"}
           name={"password"}
           label={"Password"}
           min={6}
+          ref={passwordRef}
         />
         <Button label={"Register"} variant={"primary"} type={"submit"} />
       </Form>
--- client/src/redux/slices/userSlice.ts
+++ client/src/redux/slices/userSlice.ts
@@ -1,5 +1,11 @@
 import type { IUserSlice } from "../../interfaces/IUserSlice.ts";
-import { createSlice } from "@reduxjs/toolkit";
+import {
+  createAsyncThunk,
+  createSlice,
+  type PayloadAction,
+} from "@reduxjs/toolkit";
+import type { IUser } from "../../interfaces/IUser.ts";
+import axios, { AxiosError } from "axios";
 
 const initialState: IUserSlice = {
   isAuthenticated: false,
@@ -7,11 +13,66 @@ const initialState: IUserSlice = {
   isLoading: false,
 };
 
+const userRegister = createAsyncThunk<
+  boolean,
+  { username: string; password: string },
+  { rejectValue: string }
+>("user/login", async ({ username, password }, { rejectWithValue }) => {
+  try {
+    await axios.post(`${import.meta.env.VITE_SERVER_URL}/api/auth/register`, {
+      username,
+      password,
+    });
+    return true;
+  } catch (e) {
+    const error = e as AxiosError;
+    return rejectWithValue(error.response!.data as string);
+  }
+});
+
+const userLogin = createAsyncThunk<
+  { user: IUser },
+  { username: string; password: string }
+>("user/login", async ({ username, password }, { rejectWithValue }) => {
+  try {
+    const response = await axios.post(
+      `${import.meta.env.VITE_SERVER_URL}/api/auth/login`,
+      {
+        username,
+        password,
+      },
+    );
+    return { user: response.data };
+  } catch (e) {
+    return rejectWithValue(e);
+  }
+});
+
 const userSlice = createSlice({
   name: "user",
   initialState,
   reducers: {},
-  // extraReducers: (builder) => {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(userLogin.pending, (state) => {
+        state.isLoading = true;
+        state.isAuthenticated = false;
+        state.user = null;
+      })
+      .addCase(
+        userLogin.fulfilled,
+        (state, action: PayloadAction<{ user: IUser }>) => {
+          state.user = action.payload.user;
+          state.isAuthenticated = true;
+          state.isLoading = false;
+        },
+      )
+      .addCase(userLogin.rejected, (state) => {
+        state.user = null;
+        state.isLoading = false;
+        state.isAuthenticated = false;
+      });
+  },
 });
 
-export { userSlice };
+export { userSlice, userLogin, userRegister };
