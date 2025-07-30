bbc4779c3f308b6ed8a9ac4a9eeaa21d90cecb98
Andre Barros <andrebarros7201@gmail.com>
2025-07-30 17:37:10 +0100

    fix import path case sensitivity in `App.tsx` and enhance layout structure in `App.css` with flexbox and gap adjustments

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
-import { Header } from "./Components/Header.tsx";
 import { Outlet } from "react-router-dom";
+import { Header } from "./components/Header.tsx";
 
 function App() {
   return (
