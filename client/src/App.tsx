c078c01dafe4f0c9f9f38d5f999f12b42d0c86bb
Andre Barros <andrebarros7201@gmail.com>
2025-07-31 15:58:55 +0100

    update imports in `App.tsx` and `Header.tsx` to reflect folder restructuring and ensure consistent paths

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,11 @@
 import "./App.css";
 import { Outlet } from "react-router-dom";
-import { Header } from "./components/Header.tsx";
 import { Notification } from "./components/ui/Notification.tsx";
 import { useEffect } from "react";
 import type { RootDispatch } from "./redux/store.ts";
 import { useDispatch } from "react-redux";
 import { userVerify } from "./redux/slices/userSlice.ts";
+import { Header } from "./components/Header/Header.tsx";
 
 function App() {
   const dispatch = useDispatch<RootDispatch>();
--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,8 +1,8 @@
-import { ButtonLink } from "./ui/ButtonLink";
 import { useSelector } from "react-redux";
-import type { RootState } from "../redux/store.ts";
 import { ButtonLogout } from "./ButtonLogout.tsx";
 import { HeaderLogo } from "./HeaderLogo.tsx";
+import type { RootState } from "../../redux/store.ts";
+import { ButtonLink } from "../ui/ButtonLink.tsx";
 
 const Header = () => {
   const { isAuthenticated } = useSelector((state: RootState) => state.user);
