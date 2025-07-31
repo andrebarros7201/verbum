d33289ba229c4981b11589c912eed63960141c01
Andre Barros <andrebarros7201@gmail.com>
2025-07-31 17:17:10 +0100

    refactor `fetchCurrentCommunity` to include detailed typing for payload and rejection, and adjust state update logic accordingly

--- client/src/redux/slices/currentCommunitySlice.ts
+++ client/src/redux/slices/currentCommunitySlice.ts
@@ -1,30 +1,33 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import type { ICurrentCommunitySlice } from "../../interfaces/ICurrentCommunitySlice.ts";
 import axios from "axios";
+import type { ICommunityComplete } from "../../interfaces/ICommunityComplete.ts";
+import type { IReturnNotification } from "../../interfaces/IReturnNotification.ts";
 
 const initialState: ICurrentCommunitySlice = {
   isLoading: false,
   community: null,
 };
 
-const fetchCurrentCommunity = createAsyncThunk(
-  "currentCommunity/fetch",
-  async ({ id }: { id: number }, { rejectWithValue }) => {
-    try {
-      const response = await axios.get(
-        `${import.meta.env.VITE_SERVER_URL}/api/community/${id}`,
-        {
-          withCredentials: true,
-        },
-      );
-      return response.data;
-    } catch (e) {
-      return rejectWithValue({
-        notification: { type: "error", message: "Failed to load community" },
-      });
-    }
-  },
-);
+const fetchCurrentCommunity = createAsyncThunk<
+  { community: ICommunityComplete },
+  { id: number },
+  { rejectValue: { notification: IReturnNotification } }
+>("currentCommunity/fetch", async ({ id }, { rejectWithValue }) => {
+  try {
+    const response = await axios.get(
+      `${import.meta.env.VITE_SERVER_URL}/api/community/${id}`,
+      {
+        withCredentials: true,
+      },
+    );
+    return { community: response.data };
+  } catch (e) {
+    return rejectWithValue({
+      notification: { type: "error", message: "Failed to load community" },
+    });
+  }
+});
 
 const currentCommunitySlice = createSlice({
   name: "currentCommunity",
@@ -37,7 +40,7 @@ const currentCommunitySlice = createSlice({
       })
       .addCase(fetchCurrentCommunity.fulfilled, (state, action) => {
         state.isLoading = false;
-        state.community = action.payload;
+        state.community = action.payload.community;
       })
       .addCase(fetchCurrentCommunity.rejected, (state) => {
         state.isLoading = false;
