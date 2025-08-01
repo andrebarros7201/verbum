6fe919866bd58fb92f6b3b731becc21b4355d1bf
Andre Barros <andrebarros7201@gmail.com>
2025-08-01 16:47:53 +0100

    add `Post` page and route definition for post-specific view

--- client/src/routes.tsx
+++ client/src/routes.tsx
@@ -2,6 +2,7 @@ import App from "./App";
 import { Register } from "./pages/Register.tsx";
 import { Login } from "./pages/Login.tsx";
 import { Communities } from "./pages/Communities.tsx";
+import { Post } from "./pages/Post.tsx";
 import { Community } from "./pages/Community.tsx";
 
 const routes = [
@@ -24,6 +25,10 @@ const routes = [
       {
         path: "/community/:id",
         element: <Community />,
+      },
+      {
+        path: "/post/:id",
+        element: <Post />,
       },
     ],
   },
