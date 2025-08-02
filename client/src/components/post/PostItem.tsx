307da7a6292438285e0a912d2c86258c8df0391a
Andre Barros <andrebarros7201@gmail.com>
2025-08-02 13:26:48 +0100

    add CommentItem to PostItem component

--- client/src/components/post/PostItem.tsx
+++ client/src/components/post/PostItem.tsx
@@ -1,5 +1,6 @@
 import { useSelector } from "react-redux";
 import type { RootState } from "../../redux/store.ts";
+import { CommentItem } from "./CommentItem.tsx";
 
 const PostItem = () => {
   const { isLoading, post } = useSelector(
@@ -12,8 +13,19 @@ const PostItem = () => {
 
   return (
     post && (
-      <main className={"w-full flex-1 flex flex-col gap-4 "}>
-        <h3>{post.title}</h3>
+      <main
+        className={
+          "w-full flex-1 flex flex-col items-start justify-start gap-4 "
+        }
+      >
+        <h2 className={"font-bold text-3xl"}>{post.title}</h2>
+        <p className="text-lg">{post.votes}</p>
+        <h4>Comments {post.commentsCount}</h4>
+        <div className={"w-full flex flex-col gap-4 py-4"}>
+          {post.comments.map((comment) => (
+            <CommentItem comment={comment} />
+          ))}
+        </div>
       </main>
     )
   );
