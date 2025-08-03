964aab50da76f9a682f3a71745bb535dc2e1445d
Andre Barros <andrebarros7201@gmail.com>
2025-08-03 17:21:42 +0100

    add title prop and update layout in Form component

--- client/src/components/ui/Form.tsx
+++ client/src/components/ui/Form.tsx
@@ -3,14 +3,18 @@ import type { FormEvent, ReactNode } from "react";
 type Props = {
   children: ReactNode[];
   onSubmit: (e: FormEvent) => void;
+  title: string;
 };
 
-const Form = ({ children, onSubmit }: Props) => {
+const Form = ({ children, onSubmit, title }: Props) => {
   return (
     <form
       onSubmit={onSubmit}
-      className={"flex flex-col gap-4 p-4 rounded w-full"}
+      className={
+        "flex flex-col gap-4 p-4 rounded w-full justify-center items-start"
+      }
     >
+      <h3 className={"font-bold text-xl text-amber-600"}>{title}</h3>
       {children}
     </form>
   );
