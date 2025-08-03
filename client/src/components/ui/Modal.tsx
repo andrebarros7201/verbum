e673287ed8e5575816a5d96c00a5e7bb3b36eb8b
Andre Barros <andrebarros7201@gmail.com>
2025-08-03 15:52:04 +0100

    create reusable Modal component

---
+++ client/src/components/ui/Modal.tsx
@@ -0,0 +1,25 @@
+import type { ReactNode } from "react";
+
+type Props = {
+  children: ReactNode | ReactNode[];
+  onClose: () => void;
+};
+const Modal = ({ children, onClose }: Props) => {
+  return (
+    <div
+      onClick={onClose}
+      className={
+        "w-full h-full absolute top-0 left-0 bg-gray-600/60 backdrop-blur-sm flex-1 flex flex-col justify-start items-center md:justify-center"
+      }
+    >
+      <div
+        onClick={(e) => e.stopPropagation()}
+        className={"border-2 border-amber-600 rounded"}
+      >
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export { Modal };
