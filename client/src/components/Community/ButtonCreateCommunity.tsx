bac24d1ca27491c7aa3b8aa9fbd3756962a8097d
Andre Barros <andrebarros7201@gmail.com>
2025-08-03 18:54:26 +0100

    add functionality to create a new community and handle notifications

--- client/src/components/Community/ButtonCreateCommunity.tsx
+++ client/src/components/Community/ButtonCreateCommunity.tsx
@@ -1,9 +1,44 @@
-import { useState } from "react";
+import { type FormEvent, useRef, useState } from "react";
 import { Modal } from "../ui/Modal.tsx";
 import { Button } from "../ui/Button.tsx";
+import { Form } from "../ui/Form.tsx";
+import { FormInput } from "../ui/FormInput.tsx";
+import type { RootDispatch } from "../../redux/store.ts";
+import { useDispatch } from "react-redux";
+import { createCommunity } from "../../redux/slices/communitySlice.ts";
+import { setNotification } from "../../redux/slices/notificationSlice.ts";
+import type { IReturnNotification } from "../../interfaces/IReturnNotification.ts";
 
 const ButtonCreateCommunity = () => {
+  const dispatch = useDispatch<RootDispatch>();
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const nameRef = useRef<HTMLInputElement>(null);
+  const descriptionRef = useRef<HTMLInputElement>(null);
+
+  async function handleCreateCommunity(e: FormEvent) {
+    e.preventDefault();
+    const name = nameRef.current?.value;
+    const description = descriptionRef.current?.value;
+
+    if (!name || !description) {
+      return;
+    }
+
+    try {
+      const response = await dispatch(
+        createCommunity({ name, description }),
+      ).unwrap();
+      const { notification } = response;
+      dispatch(setNotification(notification));
+      setIsModalOpen(false);
+      nameRef.current!.value = "";
+      descriptionRef.current!.value = "";
+    } catch (e) {
+      const err = e as { notification: IReturnNotification };
+      const { notification } = err;
+      dispatch(setNotification(notification));
+    }
+  }
 
   return (
     <>
@@ -14,7 +49,25 @@ const ButtonCreateCommunity = () => {
       />
       {isModalOpen && (
         <Modal onClose={() => setIsModalOpen(false)}>
-          <div className={"p-4 bg-white"}></div>
+          <div className={"w-xl"}>
+            <Form onSubmit={handleCreateCommunity} title={"Create Community"}>
+              <FormInput
+                type={"text"}
+                name={"name"}
+                label={"Name"}
+                min={3}
+                ref={nameRef}
+              />
+              <FormInput
+                type={"text"}
+                name={"description"}
+                label={"Description"}
+                min={3}
+                ref={descriptionRef}
+              />
+              <Button label={"Create"} variant={"primary"} type={"submit"} />
+            </Form>
+          </div>
         </Modal>
       )}
     </>
--- client/src/redux/slices/communitySlice.ts
+++ client/src/redux/slices/communitySlice.ts
@@ -83,6 +83,35 @@ const leaveCommunity = createAsyncThunk<
   }
 });
 
+const createCommunity = createAsyncThunk<
+  { notification: IReturnNotification; community: ICommunitySimple },
+  { name: string; description: string },
+  { rejectValue: { notification: IReturnNotification } }
+>("community/create", async ({ name, description }, { rejectWithValue }) => {
+  try {
+    const response = await axios.post(
+      `${import.meta.env.VITE_SERVER_URL}/api/community`,
+      { name, description },
+      {
+        withCredentials: true,
+      },
+    );
+    return {
+      notification: {
+        type: "success",
+        message: "Community Created",
+      },
+      community: response.data,
+    };
+  } catch (e) {
+    const error = e as AxiosError<string>;
+    const message = error.response?.data || "Something went wrong";
+    return rejectWithValue({
+      notification: { type: "error", message },
+    });
+  }
+});
+
 const communitySlice = createSlice({
   name: "community",
   initialState,
@@ -104,7 +133,17 @@ const communitySlice = createSlice({
         state.isLoading = false;
         state.communities = [];
       })
-
+      // Create Community
+      .addCase(createCommunity.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(createCommunity.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.communities.push(action.payload.community);
+      })
+      .addCase(createCommunity.rejected, (state) => {
+        state.isLoading = false;
+      })
       // Join Community
       .addCase(joinCommunity.pending, (state) => {
         state.isLoading = true;
@@ -141,4 +180,12 @@ const communitySlice = createSlice({
   },
 });
 
-export { communitySlice, fetchAllCommunities, leaveCommunity, joinCommunity };
+const { clearCommunities } = communitySlice.actions;
+export {
+  communitySlice,
+  clearCommunities,
+  createCommunity,
+  fetchAllCommunities,
+  leaveCommunity,
+  joinCommunity,
+};
