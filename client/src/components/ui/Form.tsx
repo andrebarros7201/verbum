f58b62475872fe7fe00d6c98db2e3d32ea4e9ec1
Andre Barros <andrebarros7201@gmail.com>
2025-08-05 16:08:47 +0100

    make `title` prop optional in Form component and handle undefined case

--- client/src/components/ui/Form.tsx
+++ client/src/components/ui/Form.tsx
@@ -3,7 +3,7 @@ import type { FormEvent, ReactNode } from "react";
 type Props = {
   children: ReactNode[];
   onSubmit: (e: FormEvent) => void;
-  title: string;
+  title?: string;
 };
 
 const Form = ({ children, onSubmit, title }: Props) => {
@@ -14,7 +14,7 @@ const Form = ({ children, onSubmit, title }: Props) => {
         "flex flex-col gap-4 p-4 rounded w-full justify-center items-start"
       }
     >
-      <h3 className={"font-bold text-xl text-amber-600"}>{title}</h3>
+      {title ?? <h3 className={"font-bold text-xl text-amber-600"}>{title}</h3>}
       {children}
     </form>
   );
