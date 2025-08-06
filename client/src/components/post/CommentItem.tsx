f334cce7c9494466ed3e9278dcbe7104868b9933
Andre Barros <andrebarros7201@gmail.com>
2025-08-06 17:28:41 +0100

    update CommentItem: adjust styles for improved appearance and consistency; update border color and layout of comment details

--- client/src/components/post/CommentItem.tsx
+++ client/src/components/post/CommentItem.tsx
@@ -13,15 +13,20 @@ const CommentItem = ({ comment }: Props) => {
   return (
     <main
       className={
-        "w-full flex flex-col gap-4 items-start justify-start bg-white shadow-2xl p-4 rounded border-2 border-gray-200"
+        "w-full flex flex-col gap-4 items-start justify-start bg-white shadow-2xl p-4 rounded border-2 border-sky-600"
       }
     >
-      <div className="w-full flex gap-4 items-center justify-start">
-        <p>{comment.author.username}</p>
-        <p>{comment.createdAt.split("T")[0]}</p>
-        <ButtonVote value={-1} type="comment" id={comment.id} />
-        <p>{comment.votes}</p>
-        <ButtonVote value={1} type="comment" id={comment.id} />
+      <div className="w-full flex flex-wrap gap-4 items-center justify-start font-semibold">
+        <div className={"flex gap-2 items-center"}>
+          <p>{comment.author.username}</p>
+          <p>@</p>
+          <p>{comment.createdAt.split("T")[0]}</p>
+        </div>
+        <div className={"flex gap-2 items-center"}>
+          <ButtonVote value={-1} type="comment" id={comment.id} />
+          <p>{comment.votes}</p>
+          <ButtonVote value={1} type="comment" id={comment.id} />
+        </div>
         {comment.author.id == user?.id && (
           <ButtonDeleteComment commentId={comment.id} />
         )}
