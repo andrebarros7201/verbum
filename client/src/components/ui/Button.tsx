5ea459712b2cb1eb9c332e1a690afbe5f24885c3
Andre Barros <andrebarros7201@gmail.com>
2025-08-06 15:08:07 +0100

    add size prop to Button component with small, medium, and large options

--- client/src/components/ui/Button.tsx
+++ client/src/components/ui/Button.tsx
@@ -6,6 +6,7 @@ type Props = {
   variant: "primary" | "secondary" | "danger";
   onClick?: () => void;
   isDisabled?: boolean;
+  size: "small" | "medium" | "large";
 };
 
 const Button = ({
@@ -14,17 +15,21 @@ const Button = ({
   onClick,
   type = "button",
   isDisabled = false,
+  size = "medium",
 }: Props) => {
   return (
     <button
       disabled={isDisabled}
       type={type}
       className={clsx(
-        `text-white px-4 py-2 rounded-md hover:shadow-xl transition-all duration-300`,
+        `text-white rounded-md hover:shadow-xl transition-all duration-300`,
         {
           "bg-amber-600": variant === "primary",
           "bg-gray-400": variant === "secondary",
           "bg-red-600": variant === "danger",
+          "px-4 py-2 text-sm": size === "small",
+          "px-6 py-3 text-md": size === "medium",
+          "px-8 py-4 text-lg": size === "large",
           "cursor-not-allowed": isDisabled,
           "cursor-pointer": !isDisabled,
         },
