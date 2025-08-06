70bc96f4691d2b10777d1a17bd33f4d6d5aee7f2
Andre Barros <andrebarros7201@gmail.com>
2025-08-06 18:35:25 +0100

    update ButtonLogout: add navigation to login page after successful logout

--- client/src/components/header/ButtonLogout.tsx
+++ client/src/components/header/ButtonLogout.tsx
@@ -3,15 +3,18 @@ import type { RootDispatch } from "../../redux/store.ts";
 import { Button } from "../ui/Button.tsx";
 import { setNotification } from "../../redux/slices/notificationSlice.ts";
 import { userLogout } from "../../redux/slices/userSlice.ts";
+import { useNavigate } from "react-router-dom";
 
 const ButtonLogout = () => {
   const dispatch = useDispatch<RootDispatch>();
+  const navigate = useNavigate();
 
   async function handleLogout() {
     try {
       const response = await dispatch(userLogout()).unwrap();
       const { notification } = response;
       dispatch(setNotification(notification));
+      navigate("/login");
     } catch (e: any) {
       const { notification } = e;
       dispatch(setNotification(notification));
