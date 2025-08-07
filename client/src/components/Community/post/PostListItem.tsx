a592eab52ba635bc2b0f7c83ec95898f16432dc0
Andre Barros <andrebarros7201@gmail.com>
2025-08-07 18:35:18 +0100

    update PostListItem: conditionally render DropdownMenu with delete button for post owner

--- client/src/components/Community/post/PostListItem.tsx
+++ client/src/components/Community/post/PostListItem.tsx
@@ -1,18 +1,31 @@
 import type { IPostSimple } from "../../../interfaces/IPostSimple.ts";
 import { ButtonLink } from "../../ui/ButtonLink.tsx";
+import { DropdownMenu } from "../../ui/DropdownMenu.tsx";
+import { ButtonDeletePost } from "../../post/ButtonDeletePost.tsx";
+import { useSelector } from "react-redux";
+import type { RootState } from "../../../redux/store.ts";
 
 type Props = {
   post: IPostSimple;
 };
 
 const PostListItem = ({ post }: Props) => {
+  const { user } = useSelector((state: RootState) => state.user);
+
   return (
     <div
       className={
         "w-full flex flex-col justify-start items-start gap-4 p-4 bg-white rounded border-2 border-sky-600 shadow-2xl hover:scale-105 transition-all duration-300"
       }
     >
-      <h3 className={"capitalize font-bold text-xl"}>{post.title}</h3>
+      <div className={"flex gap-4 items-center justify-between w-full"}>
+        <h3 className={"capitalize font-bold text-xl"}>{post.title}</h3>
+        {user?.id === post.user.id && (
+          <DropdownMenu>
+            <ButtonDeletePost id={post.id} userId={post.user.id} />
+          </DropdownMenu>
+        )}
+      </div>
       <p>{post.votes} Votes</p>
       <p>
         {post.commentsCount === 0
