0e2392c8079b3250af73fbdbccf0d6d81e5a328f
Andre Barros <andrebarros7201@gmail.com>
2025-08-07 18:49:43 +0100

    update FormTextArea styles: change border color from amber to sky for consistency with updated design scheme

--- client/src/components/ui/FormTextArea.tsx
+++ client/src/components/ui/FormTextArea.tsx
@@ -23,7 +23,7 @@ const FormTextArea = forwardRef<HTMLTextAreaElement, Props>(
           ref={ref}
           value={value}
           placeholder={placeholder}
-          className="p-2 bg-gray-300 rounded w-full resize-y min-h-12 text-black border-b-2 box-border outline-none border-b-amber-600 placeholder:italic appearance-none [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none [&::-moz-appearance:textfield]"
+          className="p-2 bg-gray-300 rounded w-full resize-y min-h-12 text-black border-b-2 box-border outline-none border-b-sky-600 placeholder:italic appearance-none [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none [&::-moz-appearance:textfield]"
         />
       </div>
     );
