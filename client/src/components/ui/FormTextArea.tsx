be5a41944a6bee2934034b7a6c40f59971e00815
Andre Barros <andrebarros7201@gmail.com>
2025-08-08 16:52:21 +0100

    update FormTextArea: replace `value` with `defaultValue` for uncontrolled component usage

--- client/src/components/ui/FormTextArea.tsx
+++ client/src/components/ui/FormTextArea.tsx
@@ -21,7 +21,7 @@ const FormTextArea = forwardRef<HTMLTextAreaElement, Props>(
           minLength={min}
           maxLength={max}
           ref={ref}
-          value={value}
+          defaultValue={value}
           placeholder={placeholder}
           className="p-2 bg-gray-300 rounded w-full resize-y min-h-12 text-black border-b-2 box-border outline-none border-b-sky-600 placeholder:italic appearance-none [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none [&::-moz-appearance:textfield]"
         />
