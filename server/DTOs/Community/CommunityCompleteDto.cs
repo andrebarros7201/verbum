6210716d9221cbe5fcc163330b88978bd3a2890d
Andre Barros <andrebarros7201@gmail.com>
2025-08-14 20:58:21 +0100

    update CommunityCompleteDto: add Members property to include community members list

--- server/DTOs/Community/CommunityCompleteDto.cs
+++ server/DTOs/Community/CommunityCompleteDto.cs
@@ -9,6 +9,7 @@ public class CommunityCompleteDto {
     public string Description { get; set; }
     public UserSimpleDto Owner { get; set; }
     public List<PostSimpleDto> Posts { get; set; }
+    public List<MemberDto> Members { get; set; }
     public int MembersCount { get; set; }
     public int PostsCount { get; set; }
     public bool isMember { get; set; }
