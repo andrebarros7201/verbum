2f2d7819f589eb494d402bf8dc069cd9f59773e7
Andre Barros <andrebarros7201@gmail.com>
2025-08-15 17:01:29 +0100

    create ListFilter component

--- client/src/components/ListFilter.tsx
+++ client/src/components/ListFilter.tsx
@@ -2,20 +2,51 @@ import { useEffect, useState } from "react";
 import { Button } from "./ui/Button.tsx";
 import { Modal } from "./ui/Modal.tsx";
 import { FormInput } from "./ui/FormInput.tsx";
+import { useDispatch, useSelector } from "react-redux";
+import type { RootDispatch, RootState } from "../redux/store.ts";
+import { filterCommunities } from "../redux/slices/communitySlice.ts";
+import { ListFilterCommunityItem } from "./ListFilterCommunityItem.tsx";
+import {
+  filterMembers,
+  filterPosts,
+} from "../redux/slices/currentCommunitySlice.ts";
+import { ListFilterPostItem } from "./ListFilterPostItem.tsx";
+import { ListFilterMemberItem } from "./ListFilterMemberItem.tsx";
+import type { ICommentSimple } from "../interfaces/ICommentSimple.ts";
+import type { ICommunitySimple } from "../interfaces/ICommunitySimple.ts";
 
 type Props = {
   type: "community" | "post" | "member";
 };
 
 const ListFilter = ({ type }: Props) => {
+  const { filteredCommunities } = useSelector(
+    (state: RootState) => state.community,
+  );
+  const { filteredMembers, filteredPosts } = useSelector(
+    (state: RootState) => state.currentCommunity,
+  );
   const [searchText, setSearchText] = useState("");
   const [isInputVisible, setIsInputVisible] = useState<boolean>(false);
+  const dispatch = useDispatch<RootDispatch>();
 
   useEffect(() => {
-    if (searchText?.length > 3) {
-      console.log(searchText);
+    switch (type) {
+      case "community":
+        dispatch(filterCommunities({ searchText }));
+        break;
+
+      case "post":
+        dispatch(filterPosts({ searchText }));
+        break;
+
+      case "member":
+        dispatch(filterMembers({ searchText }));
+        break;
     }
-  }, [searchText]);
+  }, [searchText, dispatch, type]);
+
+  useEffect(() => {}, []);
 
   return (
     <div
@@ -46,6 +77,20 @@ const ListFilter = ({ type }: Props) => {
               value={searchText}
               onChange={(e) => setSearchText(e.target.value)}
             />
+            <div className="w-full h-[250px] flex flex-col gap-4 justify-start items-center overflow-y-auto">
+              {type === "community" && filteredCommunities
+                ? filteredCommunities.map((x) => (
+                    <ListFilterCommunityItem community={x} key={x.id} />
+                  ))
+                : type === "post" && filteredPosts
+                  ? filteredPosts.map((x) => (
+                      <ListFilterPostItem post={x} key={x.id} />
+                    ))
+                  : filteredMembers &&
+                    filteredMembers.map((x) => (
+                      <ListFilterMemberItem member={x} key={x.id} />
+                    ))}
+            </div>
           </div>
         </Modal>
       )}
